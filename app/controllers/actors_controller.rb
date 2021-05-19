c895f2e6561a9b4dc1a4fb53414cef72526c38f1
svbergstrom <sbergstr@chicagobooth.edu>
2021-05-19 05:25:30 +0000

    simplified with no working actor filmography

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -9,7 +9,7 @@ class ActorsController < ApplicationController
     the_id = params.fetch("an_id")
 
     @the_actor = Actor.where({ :id => the_id }).at(0)
-    @filmography = Character.where({ :actor_id => the_id }).movie_id
+    
 
     render({ :template => "actor_templates/show.html.erb"})
   end
