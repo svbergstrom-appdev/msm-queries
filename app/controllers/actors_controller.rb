ed8841a292a8ad182344a82303cefce30c9c533c
svbergstrom <sbergstr@chicagobooth.edu>
2021-05-19 05:23:27 +0000

    attempts

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,14 @@ class ActorsController < ApplicationController
     @list_of_actors = Actor.all
     render({ :template => "actor_templates/index.html.erb"})
   end
+
+  def actor_details
+
+    the_id = params.fetch("an_id")
+
+    @the_actor = Actor.where({ :id => the_id }).at(0)
+    @filmography = Character.where({ :actor_id => the_id }).movie_id
+
+    render({ :template => "actor_templates/show.html.erb"})
+  end
 end
