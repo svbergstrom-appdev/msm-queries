d9bcfbd02c9c49671e0e947f806dff160063dc55
svbergstrom <sbergstr@chicagobooth.edu>
2021-05-19 04:55:27 +0000

    movies links working

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,6 @@
+class ActorsController < ApplicationController
+  def index
+    @list_of_actors = Actor.all
+    render({ :template => "actor_templates/index.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,15 @@ Rails.application.routes.draw do
 
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details" })
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
 end
