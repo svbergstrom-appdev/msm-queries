6ff724d8246d9c608f91eece78c341fa11ad278f
svbergstrom <sbergstr@chicagobooth.edu>
2021-05-19 03:02:11 +0000

    progress

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,7 +6,22 @@ class DirectorsController < ApplicationController
   end
 
   def wisest
+    @oldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
     render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest_director = Director.all.order({ :dob => :desc}).at(0)
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
+  def director_details
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+
+    render({ :template => "director_templates/show.html.erb"})
   end
 
 end
